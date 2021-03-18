748d58c94a25329c2d514d176b12431356b925c5
Yamendra Pun <punyamendra@gmail.com>
2021-03-18 17:07:16 +0900

    Forkify App added

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '404b1749-8afc-4eb5-b024-49f8a363a4d6';
+export const MODAL_CLOSE_SEC = 2.5;
